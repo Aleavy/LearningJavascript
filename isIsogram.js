fa81cbac357356b3260d4a7e1219632ff8a01b6d
aleavy <aleavy02@gmail.com>
2025-03-12 14:05:55 -0300

    Isogram exercise

---
+++ isIsogram.js
@@ -0,0 +1,19 @@
+const isisogram = (word)=> {
+    let sanitazedWord = word.toLowerCase().normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
+    sanitazedWord = sanitazedWord.replace(/\s+/g, '');
+    if (!sanitazedWord){
+        return true
+    }
+    const wordSet = new Set()
+    for (let i = 0; i < sanitazedWord.length; i++){
+        let letter = sanitazedWord[i];
+        if (wordSet.has(letter)){
+            return false;
+        }
+        wordSet.add(letter)
+    }
+    return true;
+}
+
+console.log(isisogram("  ga  to  "))
